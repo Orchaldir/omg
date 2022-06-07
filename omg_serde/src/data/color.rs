62eaace7bd8263884d01787ade3cc64a84571b7a
Orchaldir <orchaldir@gmail.com>
2022-06-07 23:25:32 +0200

    [#3] Add ColorSerde

---
+++ omg_serde/src/data/color.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Serialize};
+use omg::data::color::Color;
+
+#[derive(Serialize, Deserialize)]
+pub struct ColorSerde {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl From<ColorSerde> for Color {
+    fn from(color: ColorSerde) -> Self {
+        Color::new(color.r, color.g, color.b)
+    }
+}
+
+impl From<Color> for ColorSerde {
+    fn from(color: Color) -> Self {
+        ColorSerde {
+            r: color.r(),
+            g: color.g(),
+            b: color.b(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use omg::data::color::ORANGE;
+    use super::*;
+
+    #[test]
+    fn test_conversion() {
+        let color = ORANGE;
+        let serde: ColorSerde = color.into();
+        let result: Color = serde.into();
+
+        assert_eq!(result, color)
+    }
+}
