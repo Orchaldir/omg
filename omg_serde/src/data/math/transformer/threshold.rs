7d00bd07e6e4df7d7d578e776e8e351a2a1de895
Orchaldir <orchaldir@gmail.com>
2022-06-10 23:19:21 +0200

    [#3] Add OverwriteWithThresholdSerde

---
+++ omg_serde/src/data/math/transformer/threshold.rs
@@ -0,0 +1,37 @@
+use omg::data::input::IntInput;
+use omg::data::math::transformer::threshold::OverwriteWithThreshold;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OverwriteWithThresholdSerde<T: IntInput> {
+    value: T,
+    threshold: T,
+}
+
+impl<T: IntInput> From<OverwriteWithThresholdSerde<T>> for OverwriteWithThreshold<T> {
+    fn from(overwrite: OverwriteWithThresholdSerde<T>) -> Self {
+        OverwriteWithThreshold::new(overwrite.value, overwrite.threshold)
+    }
+}
+
+impl<T: IntInput> From<&OverwriteWithThreshold<T>> for OverwriteWithThresholdSerde<T> {
+    fn from(overwrite: &OverwriteWithThreshold<T>) -> Self {
+        OverwriteWithThresholdSerde {
+            value: overwrite.value(),
+            threshold: overwrite.threshold(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_conversion() {
+        let start = OverwriteWithThreshold::new(8, 9);
+        let serde: OverwriteWithThresholdSerde<u8> = (&start).into();
+
+        assert_eq!(OverwriteWithThreshold::from(serde), start);
+    }
+}
