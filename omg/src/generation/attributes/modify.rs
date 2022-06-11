272c598162967328b59ce9c1f6a841ae10f53df6
Orchaldir <orchaldir@gmail.com>
2022-06-11 11:19:41 +0200

    [#3] Add ModifyWithAttributeStepSerde

--- omg/src/generation/attributes/modify.rs
+++ omg/src/generation/attributes/modify.rs
@@ -1,40 +1,49 @@
 use crate::data::map::Map2d;
 
 /// Modifies one [`Attribute`] with another transformed one.
-#[derive(new, Debug, Clone)]
+#[derive(new, Debug, PartialEq, Eq, Clone)]
 pub struct ModifyWithAttributeStep {
     source_id: usize,
     target_id: usize,
-    factor: f32,
+    percentage: i32,
     minimum: u8,
 }
 
 impl ModifyWithAttributeStep {
+    pub fn source_id(&self) -> usize {
+        self.source_id
+    }
+
+    pub fn target_id(&self) -> usize {
+        self.target_id
+    }
+
+    pub fn percentage(&self) -> i32 {
+        self.percentage
+    }
+
+    pub fn minimum(&self) -> u8 {
+        self.minimum
+    }
+
     // Runs the step.
     pub fn run(&self, map: &mut Map2d) {
+        let factor = self.percentage as f32 / 100.0;
         info!(
             "{} attribute '{}' with attribute '{}' of map '{}'",
-            if self.factor < 0.0 {
-                "Decrease"
-            } else {
-                "Increase"
-            },
+            if factor < 0.0 { "Decrease" } else { "Increase" },
             map.get_attribute(self.target_id).name(),
             map.get_attribute(self.source_id).name(),
             map.name()
         );
 
-        let values = self.calculate_values(map);
+        let values = self.calculate_values(map, factor);
         let attribute = map.get_attribute_mut(self.target_id);
 
         attribute.replace_all(values);
     }
 
-    fn calculate_value(&self, source: u8, target: u8) -> u8 {
-        (target as f32 + (source.max(self.minimum) - self.minimum) as f32 * self.factor) as u8
-    }
-
-    fn calculate_values(&self, map: &mut Map2d) -> Vec<u8> {
+    fn calculate_values(&self, map: &mut Map2d, factor: f32) -> Vec<u8> {
         let length = map.size().get_area();
         let source_attribute = map.get_attribute(self.source_id);
         let target_attribute = map.get_attribute(self.target_id);
@@ -43,9 +52,13 @@ impl ModifyWithAttributeStep {
         for index in 0..length {
             let source = source_attribute[index];
             let target = target_attribute[index];
-            values.push(self.calculate_value(source, target));
+            values.push(self.calculate_value(source, target, factor));
         }
 
         values
+    }
+
+    fn calculate_value(&self, source: u8, target: u8, factor: f32) -> u8 {
+        (target as f32 + (source.max(self.minimum) - self.minimum) as f32 * factor) as u8
     }
 }
--- omg_serde/src/generation/attributes/mod.rs
+++ omg_serde/src/generation/attributes/mod.rs
@@ -2,5 +2,5 @@ pub mod create;
 pub mod distortion1d;
 pub mod distortion2d;
 pub mod generator;
-//pub mod modify;
+pub mod modify;
 //pub mod transformer;
