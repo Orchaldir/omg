7275ed66e15dc53622457064f4e717b72c87eca1
Orchaldir <orchaldir@gmail.com>
2022-06-15 20:25:10 +0200

    [#6] Improve docu

--- omg_core/src/interface/map.rs
+++ omg_core/src/interface/map.rs
@@ -1,6 +1,8 @@
 use crate::generation::MapGeneration;
 use anyhow::Result;
 
+/// This is an interface to save & load maps generators.
+/// It keeps additional dependencies like [serde](https://serde.rs) out of the core repo.
 pub trait MapStorage {
     fn read(&self, path: &str) -> Result<MapGeneration>;
 
--- omg_core/src/interface/selector.rs
+++ omg_core/src/interface/selector.rs
@@ -1,6 +1,8 @@
 use crate::data::math::selector::ColorSelector;
 use anyhow::Result;
 
+/// This is an interface to save & load color selectors.
+/// It keeps additional dependencies like [serde](https://serde.rs) out of the core repo.
 pub trait SelectorStorage {
     fn read(&self, path: &str) -> Result<ColorSelector>;
 
